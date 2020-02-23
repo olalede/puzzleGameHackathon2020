62dc5d7a59727b972a6546593dc1948d4b7a5ce7
olalede <oludareo@outlook.com>
2020-02-23 07:55:25 -0500

    cleaned up the POd files
    pod files deintegrate

--- HackathonPuzzleGame2020/Pods/Target Support Files/Pods-HackathonPuzzleGame2020-HackathonPuzzleGame2020UITests/Pods-HackathonPuzzleGame2020-HackathonPuzzleGame2020UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HackathonPuzzleGame2020_HackathonPuzzleGame2020UITests : NSObject
-@end
-@implementation PodsDummy_Pods_HackathonPuzzleGame2020_HackathonPuzzleGame2020UITests
-@end
--- HackathonPuzzleGame2020/Pods/Target Support Files/Pods-HackathonPuzzleGame2020/Pods-HackathonPuzzleGame2020-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HackathonPuzzleGame2020 : NSObject
-@end
-@implementation PodsDummy_Pods_HackathonPuzzleGame2020
-@end
